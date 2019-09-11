9d77d3588c1017438c5ec52078dac4f4d0a9bfb0
Troy Patterson <tpatterson023@gmail.com>
2019-09-10 18:23:48 -0700

    added more js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,6 @@
-let wins = 0
-let losses = 0
-let guesses = 9
-
 document.onkeyup = function (event) {
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
+    if (event.keyCode >= 65 & event.keyCode <= 90) {
         console.log(event.key)
     }
+
 }
